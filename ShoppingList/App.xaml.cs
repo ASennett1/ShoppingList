0a0a7c01d6fb9875df35b3b261114c2432c3b314
Andy Sennett <andy.sennett2167@fvtc.edu>
2024-12-01 14:21:08 -0800

    Shopping list login,new account mainpage

---
+++ ShoppingList/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using ShoppingList.Views;
+using NavigationPage = Microsoft.Maui.Controls.NavigationPage;
+
+namespace ShoppingList;
+
+public partial class App : Application
+{
+    public static string SessionKey = "";
+    
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new NavigationPage(new MainPage());
+    }
+}
